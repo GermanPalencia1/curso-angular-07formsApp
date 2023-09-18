43e5228270afc1095ec6d02b11fa077b24c5a7f2
Germán <german.palencia@adentia.es>
2023-09-18 17:40:11 +0200

    dynamic forms part 1

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
@@ -6,6 +7,34 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
+
 export class DynamicPageComponent {
+
+  public myForm: FormGroup = this.fb.group ({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favouriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ])
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  get favouriteGames() {
+    return this.myForm.get('favouriteGames') as FormArray;
+  }
+
+  onSubmit(): void {
+
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset();
+
+  }
 
 }
