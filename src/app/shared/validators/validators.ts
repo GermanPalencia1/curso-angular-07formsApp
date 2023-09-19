576c9b9ad5c8ad711292d8635a3f9fb14c4cef80
Germán <german.palencia@adentia.es>
2023-09-19 16:35:18 +0200

    Regular Expressions to validate

--- src/app/shared/validators/validators.ts
+++ src/app/shared/validators/validators.ts
@@ -1,5 +1,11 @@
 import { FormControl, ValidationErrors } from "@angular/forms";
 
+//Expresiones regulares para email:
+export const firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+export const emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+
+
 //Validación síncrona
 export const cantBeStrider = (control: FormControl): ValidationErrors | null => {
 
