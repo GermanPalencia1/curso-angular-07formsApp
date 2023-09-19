72e05b47490d82e6d39e15399a82faa3351f7387
Germán <german.palencia@adentia.es>
2023-09-19 17:51:29 +0200

    Validator service optimization

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/services/validators.service';
 
 @Component({
   selector: 'app-dynamic-page',
@@ -20,15 +21,17 @@ export class DynamicPageComponent {
 
   public newFavourite: FormControl = new FormControl ('', [Validators.required]);
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService
+  )  {}
 
   get favouriteGames() {
     return this.myForm.get('favouriteGames') as FormArray;
   }
 
   isValidField(field: string): boolean | null {
-    return this.myForm.controls[field].errors
-    && this.myForm.controls[field].touched
+    return this.validatorsService.isValidField(this.myForm, field);
   }
 
   isValidFieldInArray(formArray:FormArray, index: number): boolean | null {
--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/services/validators.service';
 
 @Component({
   selector: 'app-switches-page',
@@ -21,15 +22,17 @@ export class SwitchesPageComponent implements OnInit{
     wantNotifications: false,
   };
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService
+  )  {}
 
   ngOnInit(): void {
     this.myForm.reset(this.person);
   }
 
   isValidField(field: string): boolean | null {
-    return this.myForm.controls[field].errors
-    && this.myForm.controls[field].touched
+    return this.validatorsService.isValidField(this.myForm, field);
   }
 
   //ngSubmit
